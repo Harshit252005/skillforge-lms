b2291891da0bfdc28e8032c4e19352fcb7854b9c
Harshit252005 <harshit20051025@gmail.com>
2026-08-20 16:00:21 +0530

    Update API baseURL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://skillforge-backend-y4iw.onrender.com',
 });
 
 API.interceptors.request.use((config) => {
